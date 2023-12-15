11f9d491dd73d16a0cd68cf20205b39924452583
Stephen Lang <stephenlang91@gmail.com>
2023-12-15 18:26:46 +0100

    fixed jupyternotebook link issue

--- src/projects/models.py
+++ src/projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     git_repo = models.URLField()
-    jupyter_notebook = models.URLField()
+    jupyter_notebook = models.URLField(blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(default=None)
 
